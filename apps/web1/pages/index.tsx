fc2c0f5ec2d085af74ccd91515e7159a482c8ddf
Benedikt Jónsson <bensijons@gmail.com>
2022-09-20 22:40:33 +0000

    test

--- apps/web1/pages/index.tsx
+++ apps/web1/pages/index.tsx
@@ -12,7 +12,7 @@ export function Index() {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there! </span>
+              <span> Hello there!! </span>
             </h1>
           </div>
 
