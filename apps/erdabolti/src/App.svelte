0c07cb5032ff8ed0b6602978c80e7d66f7ec1440
Benedikt Jónsson <bensijons@gmail.com>
2022-09-28 23:09:54 +0000

    setup erdabolti project with svelte+tailwind

---
+++ apps/erdabolti/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Tailwind from "./Tailwind.svelte";
+</script>
+
+<Tailwind />
+
+<main>
+  <div class="flex items-center">
+    <h1 class="text-12xl font-bold underline">
+      Tailwind!
+    </h1>
+  </div>
+</main>
+
+<style>
+
+</style>
