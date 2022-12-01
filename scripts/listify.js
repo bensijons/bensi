ccbc661484de3679d3c8637bcc4c888301448d81
Benedikt Jónsson <bensijons@gmail.com>
2022-12-01 16:45:55 +0000

    fix

--- scripts/listify.js
+++ scripts/listify.js
@@ -1,12 +1,11 @@
-const search = '\\-';
-const dashRegExp = new RegExp(search, 'g');
-const listify = (str) =>
-  str
-    .split(',')
-    .map(
-      (item) =>
-        item.trim().toUpperCase().replace(dashRegExp, '_') +
-        '_VERCEL_PROJECT_ID'
-    );
-const list = listify(process.argv[2]);
+const mapProjects = (str) => {
+  const result = {};
+  result.project = str?.split(',').map((project) => project?.trim()) || [];
+  result.include = result.project.map((project) => ({
+    project,
+    key: project.toUpperCase().replaceAll('-', '_'),
+  }));
+  return result;
+};
+const list = mapProjects(process.argv[2]);
 process.stdout.write(JSON.stringify(list));
