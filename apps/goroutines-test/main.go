b87485bc319bc41000b07c0f087a264880316c33
Benedikt Jónsson <bensijons@gmail.com>
2023-01-08 13:02:52 +0000

    testing goroutines

---
+++ apps/goroutines-test/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func Hello(name string) string {
+	result := "Hello " + name
+	return result
+}
+
+func CreateFile(filenumber string, c chan string, closeChan bool) {
+	time.Sleep(time.Second)
+	cwd, _ := os.Getwd()
+
+	err := os.MkdirAll("test", os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := fmt.Sprintf("test%s.txt", filenumber)
+	path := filepath.Join(cwd, "test", filename)
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	c <- filename
+	if closeChan {
+		close(c)
+	}
+	fmt.Println("success!")
+}
+
+func main() {
+	c := make(chan (string))
+	go CreateFile("1", c, false)
+	go CreateFile("2", c, false)
+	go CreateFile("3", c, false)
+	go CreateFile("4", c, false)
+	go CreateFile("5", c, true)
+	for msg := range c {
+		fmt.Println(msg)
+	}
+	fmt.Println("DONE")
+}
